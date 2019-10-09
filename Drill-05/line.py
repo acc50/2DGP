8fbd2267ea137d19bbde72893bf7b1eaa879c77b
LeeDongHyun <aliod12@gmail.com>
2019-10-09 16:36:23 +0900

    Drill-05 캐릭터 이동
    클릭 시 캐릭터 이동 구현
    캐릭터의 이동 방향에 따른 바라보는 방향 구현

--- Drill-05/line.py
+++ Drill-05/line.py
@@ -4,9 +4,25 @@ import random
 Width, Height = 800, 600
 
 
+def move_character(p1, p2):
+    global character_x, character_y
+    global dir
+
+    if character_x <= hand_x:
+        dir = 1
+    elif character_x > hand_x:
+        dir = 0
+
+    for i in range(0, 100 + 1, 2):
+        t = i / 100
+        character_x = (1 - t) * p1[0] + t * p2[0]
+        character_y = (1 - t) * p1[1] + t * p2[1]
+
+    pass
+
+
 def handle_events():
     global running
-    global character_x, character_y
     global hand_x, hand_y
     events = get_events()
 
@@ -15,17 +31,10 @@ def handle_events():
             running = False
         elif event.type == SDL_MOUSEMOTION:
             hand_x, hand_y = event.x, Height - 1 - event.y
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            move_character((character_x, character_y), (hand_x, hand_y))
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
-
-    pass
-
-
-def draw_line(p1, p2):
-    for i in range(0, 100 + 1, 2):
-        t = i / 100
-        x = (1 - t) * p1[0] + t * p2[0]
-        y = (1 - t) * p1[1] + t * p2[1]
 
     pass
 
@@ -39,13 +48,13 @@ running = True
 character_x, character_y = Width // 2, Height // 2
 hand_x, hand_y = Width // 2, Height // 2
 frame_x = 0
-frame_y = 1
+dir = 1                     # 캐릭터의 방향
 hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(Width // 2, Height // 2)
-    character.clip_draw(frame_x * 100, frame_y * 100, 100, 100, character_x, character_y)
+    character.clip_draw(frame_x * 100, dir * 100, 100, 100, character_x, character_y)
     hand_arrow.draw(hand_x, hand_y)
     update_canvas()
     frame_x = (frame_x + 1) % 8
