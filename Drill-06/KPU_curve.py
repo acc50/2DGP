72d169695a0e7b5970d15bbce07b5be02a51bcf0
LeeDongHyun <aliod12@gmail.com>
2019-10-11 14:43:46 +0900

    Drill-06 중간 커밋3, 점10개 이동
    캐릭터가 점 10개를 부드럽게 이동하는것 구현

--- Drill-06/KPU_curve.py
+++ Drill-06/KPU_curve.py
@@ -17,38 +17,15 @@ def handle_events():
     pass
 
 
-def draw_curve_4points(p1, p2, p3, p4):
-    # draw p1-p2
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t ** 3 + 2 * t ** 2 - t) * p4[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[0] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p2[0] + (t ** 3 - t ** 2) * p3[0]) / 2
-        y = ((-t ** 3 + 2 * t ** 2 - t) * p4[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[1] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p2[1] + (t ** 3 - t ** 2) * p3[1]) / 2
+def move_curve_4points(p1, p2, p3, p4, distance, speed):
+    global x, y
 
-    # draw p2-p3
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
-        y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
-
-    # draw p3-p4
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t ** 3 + 2 * t ** 2 - t) * p2[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[0] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p4[0] + (t ** 3 - t ** 2) * p1[0]) / 2
-        y = ((-t ** 3 + 2 * t ** 2 - t) * p2[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[1] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p4[1] + (t ** 3 - t ** 2) * p1[1]) / 2
-
-    # draw p4-p1
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t ** 3 + 2 * t ** 2 - t) * p3[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[0] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p1[0] + (t ** 3 - t ** 2) * p2[0]) / 2
-        y = ((-t ** 3 + 2 * t ** 2 - t) * p3[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[1] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p1[1] + (t ** 3 - t ** 2) * p2[1]) / 2
+    # move p1-p2
+    t = distance / speed
+    x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
+            -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
+    y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
+            -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
 
     pass
 
@@ -61,15 +38,26 @@ running = True
 random_points = [(random.randint(1, 1200), random.randint(1, 1000)) for n in range(10)]
 x, y = random_points[0][0], random_points[0][1]
 frame = 0
+i = 0
+n = 0
+ratio = 1000
+character_dir = 1
 hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(Width // 2, Height // 2)
-    character.clip_draw(frame * 100, 1 * 100, 100, 100, x, y)
+    character.clip_draw(frame * 100, character_dir * 100, 100, 100, x, y)
     update_canvas()
 
     handle_events()
     frame = (frame + 1) % 8
+    move_curve_4points(random_points[n % 10], random_points[(n+1) % 10],
+                       random_points[(n+2) % 10], random_points[(n+3) % 10], i, ratio)
+    i += 1
+    if i > ratio:
+        i = 0
+        n += 1
+
 
 close_canvas()
