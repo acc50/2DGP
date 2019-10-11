47f2d09e7f67c007d72c8bba05f097f2022c8c8b
LeeDongHyun <aliod12@gmail.com>
2019-10-11 14:22:12 +0900

    Drill-06
    8-1 완료
    4점의 부드러운 이동 해결

---
+++ Drill-06/KPU_curve.py
@@ -0,0 +1,72 @@
+import turtle
+import random
+
+Width, Height = 1280, 1024
+
+
+def draw_big_point(p):
+    turtle.goto(p)
+    turtle.color(0.8, 0.9, 0)
+    turtle.dot(15)
+    turtle.write('     '+str(p))
+
+
+def draw_big_4_point(p1, p2, p3, p4):
+    draw_big_point(p1)
+    draw_big_point(p2)
+    draw_big_point(p3)
+    draw_big_point(p4)
+
+
+def draw_point(p):
+    turtle.goto(p)
+    turtle.dot(5, random.random(), random.random(), random.random())
+
+
+def draw_curve_4points(p1, p2, p3, p4):
+    # draw p1-p2
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p4[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[0] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p2[0] + (t ** 3 - t ** 2) * p3[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p4[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[1] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p2[1] + (t ** 3 - t ** 2) * p3[1]) / 2
+        draw_point((x, y))
+    draw_point(p3)
+
+    # draw p2-p3
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
+        y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
+        draw_point((x, y))
+    draw_point(p3)
+
+    # draw p3-p4
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p2[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[0] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p4[0] + (t ** 3 - t ** 2) * p1[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p2[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[1] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p4[1] + (t ** 3 - t ** 2) * p1[1]) / 2
+        draw_point((x, y))
+    draw_point(p3)
+
+    # draw p4-p1
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p3[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[0] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p1[0] + (t ** 3 - t ** 2) * p2[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p3[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[1] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p1[1] + (t ** 3 - t ** 2) * p2[1]) / 2
+        draw_point((x, y))
+    draw_point(p3)
+
+    pass
+
+
+points = [(-300, 200), (400, 350), (300, -300), (-200, -200)]
+draw_big_4_point(points[0], points[1], points[2], points[3])
+
+while True:
+    draw_curve_4points(points[1], points[2], points[3], points[0])
