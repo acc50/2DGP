d664709244903789dcf55f3cd56862d6c43acdc2
LeeDongHyun <aliod12@gmail.com>
2019-10-11 14:33:01 +0900

    Drill-06 중간커밋
    배경이미지, 캐릭터 이미지 띄우기
    10개의 점 랜덤 생성

--- Drill-06/KPU_curve.py
+++ Drill-06/KPU_curve.py
@@ -1,26 +1,20 @@
-import turtle
+from pico2d import *
 import random
 
 Width, Height = 1280, 1024
 
 
-def draw_big_point(p):
-    turtle.goto(p)
-    turtle.color(0.8, 0.9, 0)
-    turtle.dot(15)
-    turtle.write('     '+str(p))
+def handle_events():
+    global running
+    events = get_events()
 
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
 
-def draw_big_4_point(p1, p2, p3, p4):
-    draw_big_point(p1)
-    draw_big_point(p2)
-    draw_big_point(p3)
-    draw_big_point(p4)
-
-
-def draw_point(p):
-    turtle.goto(p)
-    turtle.dot(5, random.random(), random.random(), random.random())
+    pass
 
 
 def draw_curve_4points(p1, p2, p3, p4):
@@ -28,19 +22,17 @@ def draw_curve_4points(p1, p2, p3, p4):
     for i in range(0, 100, 2):
         t = i / 100
         x = ((-t ** 3 + 2 * t ** 2 - t) * p4[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[0] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p2[0] + (t ** 3 - t ** 2) * p3[0]) / 2
+                -3 * t ** 3 + 4 * t ** 2 + t) * p2[0] + (t ** 3 - t ** 2) * p3[0]) / 2
         y = ((-t ** 3 + 2 * t ** 2 - t) * p4[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[1] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p2[1] + (t ** 3 - t ** 2) * p3[1]) / 2
-        draw_point((x, y))
-    draw_point(p3)
+                -3 * t ** 3 + 4 * t ** 2 + t) * p2[1] + (t ** 3 - t ** 2) * p3[1]) / 2
 
     # draw p2-p3
     for i in range(0, 100, 2):
         t = i / 100
-        x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
-        y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
-        draw_point((x, y))
-    draw_point(p3)
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
 
     # draw p3-p4
     for i in range(0, 100, 2):
@@ -49,8 +41,6 @@ def draw_curve_4points(p1, p2, p3, p4):
                 -3 * t ** 3 + 4 * t ** 2 + t) * p4[0] + (t ** 3 - t ** 2) * p1[0]) / 2
         y = ((-t ** 3 + 2 * t ** 2 - t) * p2[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[1] + (
                 -3 * t ** 3 + 4 * t ** 2 + t) * p4[1] + (t ** 3 - t ** 2) * p1[1]) / 2
-        draw_point((x, y))
-    draw_point(p3)
 
     # draw p4-p1
     for i in range(0, 100, 2):
@@ -59,14 +49,27 @@ def draw_curve_4points(p1, p2, p3, p4):
                 -3 * t ** 3 + 4 * t ** 2 + t) * p1[0] + (t ** 3 - t ** 2) * p2[0]) / 2
         y = ((-t ** 3 + 2 * t ** 2 - t) * p3[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[1] + (
                 -3 * t ** 3 + 4 * t ** 2 + t) * p1[1] + (t ** 3 - t ** 2) * p2[1]) / 2
-        draw_point((x, y))
-    draw_point(p3)
 
     pass
 
 
-points = [(-300, 200), (400, 350), (300, -300), (-200, -200)]
-draw_big_4_point(points[0], points[1], points[2], points[3])
+open_canvas(Width, Height)
+kpu_ground = load_image('KPU_GROUND.png')
+character = load_image('animation_sheet.png')
 
-while True:
-    draw_curve_4points(points[1], points[2], points[3], points[0])
+running = True
+random_points = [(random.randint(1, 1200), random.randint(1, 1000)) for n in range(10)]
+x, y = random_points[0][0], random_points[0][1]
+frame = 0
+hide_cursor()
+
+while running:
+    clear_canvas()
+    kpu_ground.draw(Width // 2, Height // 2)
+    character.clip_draw(frame * 100, 1 * 100, 100, 100, x, y)
+    update_canvas()
+
+    handle_events()
+    frame = (frame + 1) % 8
+
+close_canvas()
